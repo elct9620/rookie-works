23064458748798d8c2adc4ab17d502620c870a3e
蒼時弦也 <elct9620@frost.tw>
2018-02-20 23:28:43 +0800

    Apply basic home page view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def fullscreen?
+    return unless params[:controller] == 'devise/sessions'
+    unless params[:controller] == 'devise/registrations'
+      return unless params[:action] == 'new'
+    end
+    true
+  end
 end
