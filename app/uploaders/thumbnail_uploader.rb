926f9abb12090da35dd7eeed1e895f01e41253a6
蒼時弦也 <elct9620@frost.tw>
2018-02-21 23:01:50 +0800

    Force rename uploaded files

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -47,7 +47,8 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here,
   # see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    ext = original_filename.split('.').last
+    "thumbnail.#{ext}"
+  end
 end
