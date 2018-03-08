7276ce78ecbebcc92853d3d77ca17ba277e909a7
蒼時弦也 <elct9620@frost.tw>
2018-03-08 18:38:18 +0800

    Ensure upload files are shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 append :linked_files, 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'node_modules', 'public/shared'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'node_modules', 'public/shared', 'public/uploads'
 
 # Default value for default_env is {}
 set :default_env, { path: '/usr/local/ruby-2.3.6/bin:$PATH' }
