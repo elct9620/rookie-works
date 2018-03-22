29a6fdd5d2f9c06f1106bd7d7d686e9a2fc9927d
蒼時弦也 <elct9620@frost.tw>
2018-03-22 10:24:54 +0800

    Add filter by platform support

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,29 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  before_action :picked_platform
+
+  # TODO: Improve filter using FilterService or Search Model
   def index
-    @q =
-      Project
-      .ransack(params[:q])
+    @q = Project.ransack(params[:q])
+    @projects = @q.result(distinct: true)
+
+    if @picked_platform.any?
+      @projects = @projects.where(result: Game.platform_is(@picked_platform))
+    end
+
     @projects =
-      @q
-      .result(distinct: true)
+      @projects
       .includes(:school, :department)
       .page(current_page)
       .per(50) # TODO: Let it changable
+  end
+
+  private
+
+  def picked_platform
+    @picked_platform ||=
+      (params[:q]&.delete(:result_of_Game_type_platform_in) || [])
+      .reject(&:blank?)
   end
 end
