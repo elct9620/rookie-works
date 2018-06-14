57786ea3821a32f7c44879f09e285345ffea8f26
蒼時弦也 <elct9620@frost.tw>
2018-06-14 17:26:44 +0800

    Add post admin feature

---
+++ app/admin/posts.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Post do
+  permit_params :title, :status
+
+  index do
+    index_column
+    column :title
+    column :category
+    column :status
+    column :published_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.semantic_errors
+      f.input :title
+      f.input :status
+    end
+    panel 'Thumbnail' do
+      image_tag f.object.thumbnail_url(:thumb)
+    end
+    panel 'Preview' do
+      simple_format f.object.content
+    end
+    f.actions
+  end
+end
