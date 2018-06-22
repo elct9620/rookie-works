3426d12f1d8476d5dca5ccbc5ae704fefdefdfe1
蒼時弦也 <elct9620@frost.tw>
2018-06-22 12:55:50 +0800

    Add experience published notify mail

--- app/mailers/experience_mailer.rb
+++ app/mailers/experience_mailer.rb
@@ -8,6 +8,19 @@ class ExperienceMailer < ApplicationMailer
       'experience_mailer.new_experience_submit',
       title: @experience.title
     )
-    mail(to: 'contact@rookie.works', subject: subject)
+
+    # TODO: Add config to setup reviewer
+    mail(to: 'reviewer@rookie.works', subject: subject)
+  end
+
+  def notify_published(experience)
+    @experience = experience
+
+    subject = I18n.t(
+      'experience_mailer.notify_published.published',
+      title: @experience.title
+    )
+
+    mail(to: @experience.user.email, subject: subject)
   end
 end
