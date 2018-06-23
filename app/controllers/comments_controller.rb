db4c1897eb019c6db3cc0d8252003d11b6debee4
蒼時弦也 <elct9620@frost.tw>
2018-06-24 00:26:52 +0800

    Prevent guest leave comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
 
   RESOURCE_RULE = %r{\/([^\/]+)\/(\d+)\/comments}
 
