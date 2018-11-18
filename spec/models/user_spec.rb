b270d4adccc7fcef5418b85cb6a2bff5ec9c36f0
蒼時弦也 <elct9620@frost.tw>
2018-11-18 15:52:54 +0800

    Add user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,39 @@ RSpec.describe User, type: :model do
   it { should have_many(:games) }
   it { should have_many(:posts) }
   it { should have_many(:bookmarks) }
+
+  describe '#display_name' do
+    let(:display_name) { nil }
+    let(:email) { 'user@example.com' }
+    let(:user) { create(:user, email: email, display_name: display_name) }
+    subject { user.display_name }
+
+    it 'return email if not configured' do
+      should eq('user')
+    end
+
+    context 'is configured' do
+      let(:display_name) { 'Example' }
+
+      it 'return user name' do
+        should eq(display_name)
+      end
+    end
+  end
+
+  describe '#avatar_url' do
+    let(:email) { 'user@example.com' }
+    let(:hash) { Digest::MD5.hexdigest(email) }
+    let(:user) { create(:user, email: email) }
+    subject { user.avatar_url }
+
+    it { should eq("https://www.gravatar.com/avatar/#{hash}?s=100") }
+
+    context 'avatar size is specified' do
+      let(:size) { 150 }
+      subject { user.avatar_url(size) }
+
+      it { should eq("https://www.gravatar.com/avatar/#{hash}?s=#{size}") }
+    end
+  end
 end
