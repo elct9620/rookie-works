15cdf5617d492614c10e5e1a484cf6baa22999e5
蒼時弦也 <elct9620@frost.tw>
2018-12-25 00:28:34 +0800

    Add filter to filter non this year projects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@
 
 class HomeController < ApplicationController
   before_action :picked_platform
+  helper_method :current_year
 
   # TODO: Improve filter using FilterService or Search Model
   def index
@@ -10,13 +11,8 @@ class HomeController < ApplicationController
     @projects = @q.result(distinct: true)
 
     include_platform if picked_platform.any?
-
-    @projects =
-      @projects
-      .includes(:school, :department)
-      .latest
-      .page(current_page)
-      .per(50) # TODO: Let it changable
+    filter_default_years if params[:q].nil?
+    load_projects
   end
 
   private
@@ -29,5 +25,22 @@ class HomeController < ApplicationController
     @picked_platform ||=
       (params[:q]&.delete(:result_of_Game_type_platform_in) || [])
       .reject(&:blank?)
+  end
+
+  def filter_default_years
+    @projects = @projects.where(published_year: current_year)
+  end
+
+  def load_projects
+    @projects =
+      @projects
+      .includes(:school, :department)
+      .latest
+      .page(current_page)
+      .per(50) # TODO: Let it changable
+  end
+
+  def current_year
+    Date.current.year + (Date.current.month / 12.0).round
   end
 end
