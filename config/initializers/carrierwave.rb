f6187bf28ae39358d4a0e36826cbbdb5879c7548
蒼時弦也 <elct9620@frost.tw>
2021-12-29 18:27:11 +0800

    Disable CarrierWave processing when running test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,4 +3,7 @@
 CarrierWave.configure do |config|
   config.asset_host = 'https://rookie.works' if Rails.env.production?
   config.asset_host = 'https://beta.rookie.works' if Rails.env.staging?
+
+  # TODO: Replace CarrierWave to ActiveStorage
+  config.enable_processing = false if Rails.env.test?
 end
